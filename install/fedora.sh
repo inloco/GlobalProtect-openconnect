f147576ca4f8eb5e8df5c21771f63bbfdf4b0542
João Paulo <paulolunaoficial@gmail.com>
2020-04-20 21:35:47 -0300

    Update fedora.sh

--- install/fedora.sh
+++ install/fedora.sh
@@ -3,10 +3,10 @@ sudo dnf groupinstall "Development Tools" -y
 sudo dnf install -y qt5-qtbase-devel \
  qt5-qtwebsockets-devel qt5-qtwebengine-devel
 cd $(mktemp -d)
-curl https://github.com/PauloLuna/GlobalProtect-openconnect/archive/v1.2.2.zip \
- -o GlobalProtect-openconnect-1.2.2.zip -L
-unzip GlobalProtect-openconnect-1.2.2.zip
-cd GlobalProtect-openconnect-1.2.2
+curl https://github.com/PauloLuna/GlobalProtect-openconnect/archive/v1.3.0.zip \
+ -o GlobalProtect-openconnect-1.3.0.zip -L
+unzip GlobalProtect-openconnect-1.3.0.zip
+cd GlobalProtect-openconnect-1.3.0
 qmake-qt5 CONFIG+=release
 make
 sudo make install
