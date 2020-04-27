e3d5326163993faa406f45f7a17b42aec3b2c408
João Paulo <paulo.luna@inlocomedia.com>
2020-04-27 20:04:05 -0300

    Update ubuntu.sh

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 sudo apt install -y qt5-default libqt5websockets5-dev qtwebengine5-dev
 cd $(mktemp -d)
-curl https://github.com/inloco/GlobalProtect-openconnect/archive/v1.4.0.zip \
- -o GlobalProtect-openconnect-1.4.0.zip -L
-unzip GlobalProtect-openconnect-1.4.0.zip
-cd GlobalProtect-openconnect-1.4.0
+curl https://github.com/inloco/GlobalProtect-openconnect/archive/stable.zip \
+ -o GlobalProtect-openconnect-stable.zip -L
+unzip GlobalProtect-openconnect-stable.zip
+cd GlobalProtect-openconnect-stable
 qmake CONFIG+=release
 make
 sudo make install
