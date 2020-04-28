e31f20aa42fa183c7b935a4be1c4919dc2affdee
João Paulo <paulo.luna@inlocomedia.com>
2020-04-28 20:29:47 -0300

    Update fedora.sh

--- install/fedora.sh
+++ install/fedora.sh
@@ -6,7 +6,7 @@ cd $(mktemp -d)
 curl https://github.com/inloco/GlobalProtect-openconnect/archive/v1.4.2.zip \
  -o GlobalProtect-openconnect-v1.4.2.zip -L
 unzip GlobalProtect-openconnect-v1.4.2.zip
-cd GlobalProtect-openconnect-v1.4.2
+cd GlobalProtect-openconnect-1.4.2
 qmake-qt5 CONFIG+=release
 make
 sudo make install
